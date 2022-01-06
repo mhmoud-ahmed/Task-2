21bd137e0480b7dab412ab28c1e65aa79454ffb9
mhmoud-ahmed <mhmoudkingmhmoud@gmail.com>
2022-01-06 04:35:38 +0200

    task2

---
+++ tabletask.php
@@ -0,0 +1,121 @@
+<?php
+// dynamic table
+// dynamic rows
+// dynamic columns
+// check if gender of user == m ==> male
+// check if gender of user == f ==> female
+
+// collection => laravel => array of objects
+$users = [
+  (object)[
+    'id' => 1,
+    'name' => 'ahmed',
+    "gender" => (object)[
+      'gender' => 'm'
+    ],
+    'hobbies' => [
+      'football', 'swimming', 'running'
+    ],
+    'activities' => [
+      "school" => 'drawing',
+      'home' => 'painting'
+    ]
+  ],
+  (object)[
+    'id' => 2,
+    'name' => 'mohamed',
+    "gender" => (object)[
+      'gender' => 'm'
+    ],
+    'hobbies' => [
+      'swimming', 'running',
+    ],
+    'activities' => [
+      "school" => 'painting',
+      'home' => 'drawing'
+    ]
+  ],
+  (object)[
+    'id' => 3,
+    'name' => 'menna',
+    "gender" => (object)[
+      'gender' => 'f'
+    ],
+    'hobbies' => [
+      'running',
+    ],
+    'activities' => [
+      "school" => 'painting',
+      'home' => 'drawing'
+    ]
+  ],
+];
+// check gender 
+for ($i = 0; $i < count($users); $i++) {
+  $genders = $users[$i]->gender->gender;
+  if ($genders == 'f') {
+    $genders = $users[$i]->gender->gender = 'Female';
+  } else {
+    $genders = $users[$i]->gender->gender = 'Male';
+  }
+}
+$table = "<table class=\"table\">";
+$table .= "<thead>";
+$table .= "<tr scope=\"col\">";
+foreach ($users[0] as $index => $user) {
+  $table .= "<th scope=\"col\">";
+  $table .=  $index;
+  $table .= "</th>";
+}
+$table .= "</tr>";
+$table .= "</thead>";
+$table .= "<tbody>";
+foreach ($users as $index => $value) {
+  $table .= "<tr>";
+  foreach ($users[$index] as $key => $user) {
+    if ($key == 'gender') {
+      for ($i = 0; $i < count($users); $i++) {
+        $genders = $users[$i]->gender->gender;
+         echo $genders;
+      }
+      continue;
+    }
+    if ($key == 'hobbies') {
+      continue;
+    }
+    if ($key == 'activities') {
+      continue;
+    }
+    $table .= "<td>";
+    $table .= $user;
+    $table .= "</td>";
+  }
+  $table .= "</tr>";
+}
+$table .= "</tr>";
+$table .= "</tbody>";
+$table .= "</table>";
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+  <?= $table ?>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
